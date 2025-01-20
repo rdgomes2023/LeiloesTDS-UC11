2ab54a3e718e4f524560d32e657b9827a862664e
rdgomes2023 <rdgomes@gmail.com>
2025-01-20 17:37:33 -0300

    Implementação Método venderProduto
    Implementamos o método de vender Produtos pelo id do mesmo

--- LeiloesTDSat/LeiloesTDSat/src/dao/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/dao/ProdutosDAO.java
@@ -4,12 +4,10 @@ package dao;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
-
 import model.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
@@ -18,9 +16,8 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class ProdutosDAO {
-    
+
     private Connection conn;
     private ConectaDAO conexao;
     private PreparedStatement prep;
@@ -31,9 +28,8 @@ public class ProdutosDAO {
         this.conexao = new ConectaDAO();
         this.conn = conexao.connectDB();
     }
-    
-    
-    public void cadastrarProduto (ProdutosDTO produto){
+
+    public void cadastrarProduto(ProdutosDTO produto) {
         String sql = "INSERT INTO produtos(nome, valor, status) VALUES(?,?,?)";
         try {
             prep = this.conn.prepareStatement(sql);
@@ -47,8 +43,8 @@ public class ProdutosDAO {
             System.out.println("Erro ao inserir empresa: " + e.getMessage());
         }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         String sql = "SELECT * FROM produtos";
 
         try {
@@ -73,9 +69,28 @@ public class ProdutosDAO {
             return null;
         }
     }
-    
-    
-    
-        
-}
 
+    public void venderProduto(int id_produto) {
+        String sql = "UPDATE produtos SET status = ? WHERE id = ?";
+
+        try {
+            prep = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            //Setando os parâmetros
+            prep.setString(1, "Vendido");
+            prep.setInt(2, id_produto); //pegando o parâmetro passado pelo usuários.
+            //criando variável de condição e Executando a query
+            int result = prep.executeUpdate();
+            if (result > 0) {
+                JOptionPane.showMessageDialog(null, "Produto de Código: " + id_produto + ", VENDIDO!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Produto de Código: " + id_produto + ", não encontrado!");
+            }
+
+            //tratando o erro, caso ele ocorra.
+        } catch (Exception e) {
+            System.out.println("Erro ao vender produto: " + e.getMessage());
+        }
+    }
+
+}
